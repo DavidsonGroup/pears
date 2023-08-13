8379b95c0f3e4f72a4816704f73a62ab578253e9
shuyi-w <shuyiwu@protonmail.com>
2023-08-13 15:53:08 +1000

    downloading modules

---
+++ modules/init_tools.sh
@@ -0,0 +1,8 @@
+#git clone https://github.com/suhrig/arriba
+#cd ./arriba && make
+#cd ..
+git clone https://github.com/alexdobin/STAR
+cd ./STAR/source && make
+cd ../../
+git clone https://github.com/DavidsonGroup/flexiplex
+cd ./flexiplex && make
